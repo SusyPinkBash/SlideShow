dae465bda9d2149a474815fe4a373632e9390643
Susanna Ardigo <ardigs@usi.ch>
2019-04-17 22:44:50 +0200

    This programmer is so lazy that is pushin with an autocommit

--- Slideshow/Slideshow/slideshow.c
+++ Slideshow/Slideshow/slideshow.c
@@ -34,6 +34,11 @@ struct tag {
     struct tag * next;
 };
 
+//typedef struct {
+//    char * string;
+//    int len;
+//} tuple;
+
 // ########## HELPER FUNCTIONS ##########
 /* Given a char * buffer it clears it overriding it with '\0's */
 void clear_buffer(char * buffer, size_t len) {
@@ -49,7 +54,38 @@ void copy_char_from_buffer(char * from, char * to, char end) {
         to[i] = from[i];
         ++i;
     }
+    to[i] = '\0';
+}
+
+/* Copies a char data inside another char data with the end char without an end character*/
+void get_len_and_copy_char(char * from, char * to, char end, int to_len) {
+    int i=0;
+    while (from[i] != end) {
+        if (i >= to_len) {
+            to_len += 10;
+            to = realloc(to, to_len*sizeof(char));
+        }
+        to[i] = from[i];
+        ++i;
+    }
     from[i] = '\0';
+    to = realloc(to, i);
+}
+
+/* Copies a char data inside another char data with the end char without an end character*/
+int get_len_copy_char(char * from, char * to, char end, int to_len) {
+    int i=0;
+    while (from[i] != end && from[i] != '\0') {
+        if (i >= to_len) {
+            to_len += 10;
+            to = realloc(to, to_len*sizeof(char));
+        }
+        to[i] = from[i];
+        ++i;
+    }
+    to[i] = '\0';
+    to = realloc(to, i+1);
+    return i;
 }
 
 
@@ -68,24 +104,57 @@ void file_to_buffer(FILE * file, char * buffer, char end, int buff_len) {
 }
 
 orientation get_orientation(char * line) {
-    orientation or = (line[0] == 'H') ? H : V;
-    return or;
+    return (line[0] == 'H') ? H : V;
 }
 
 int get_n_tags(char * line) {
     char * buffer = malloc(10*sizeof(char));
     copy_char_from_buffer(line, buffer, ' ');
+    free(buffer);
     return atoi(buffer);
+}
+
+int get_index(char * line) {
+    int i = 0;
+    while(line[i] != ' ') {
+        ++i;
+    }
+    return ++i;
 }
 
 
 
 // ########## CREATE STRUCTS FUNCTIONS ##########
 /* Constructor: returns a null pointer in case of failure/
+ * Creates a Tag from a given char *
+ */
+struct tag * tag_new(char * line) {
+    struct tag * this = malloc(sizeof(struct tag));
+    if (!this) {
+        return NULL;
+    }
+    /* index to iteate the line char * */
+    this->photo = NULL;
+    
+    char * name = malloc(10*sizeof(char));
+    int offset = get_len_copy_char(line, name, ' ', 10);
+    this->hashtag = name;
+//    printf("%c\n", &line[offset]);
+    if (line[offset] != '\0'){
+        this->next = tag_new(&line[++offset]);
+    }
+    else {
+        this->next = NULL;
+    }
+    return this;
+}
+
+
+/* Constructor: returns a null pointer in case of failure/
  * Creates a Photo from a given char *
  */
 struct photo * photo_new(char * line, int photoID) {
-    printf("%s\n", line);
+//    printf("%s\n", line);
     struct photo * this = malloc(sizeof(struct photo));
     if (!this) {
         return NULL;
@@ -93,8 +162,16 @@ struct photo * photo_new(char * line, int photoID) {
     this->photoID = photoID;
     this->orientation = get_orientation(line);
     this->n_of_tags = get_n_tags(&line[2]);
-    this->first_tag = NULL;
-    this->next = NULL;
+    this->first_tag = NULL; // TODO: create tag
+    this->first_tag = tag_new(&line[3+get_index(&line[3])]);
+//    this->first_tag->photo = this; // TODO: add photo to all tag structs
+    struct tag * current = this->first_tag;
+    while (current->next != NULL) {
+        current->photo = this;
+        current = current->next;
+    }
+    
+    this->next = NULL; // TODO: next photo
     return this;
 }
 
@@ -119,9 +196,9 @@ struct photoset * ps_new(char * filename) {
     this->first_photo = NULL; // TODO: create photos
     char * line = malloc(100*sizeof(char));
     file_to_buffer(file, line, '\n', 100);
-//    printf("%s", line);
-    
     this->first_photo = photo_new(line, 0);
+//    struct photo * prev = this->first_photo;
+//    for (int i=0; i < )
     
     return this;
 }
